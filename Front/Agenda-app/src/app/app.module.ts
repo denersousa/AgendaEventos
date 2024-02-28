b08b4ac8a9658c780f1e8a8dbc68bd7d866f823b
Dener Sousa <90757341+denersousa@users.noreply.github.com>
2024-02-28 11:18:32 -0300

    Apresentação da grid com os eventos

--- Front/Agenda-app/src/app/app.module.ts
+++ Front/Agenda-app/src/app/app.module.ts
@@ -6,18 +6,26 @@ import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
-    PalestrantesComponent
-  ],
+    PalestrantesComponent,
+    NavComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-  ],
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule
+ ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- Front/Agenda-app/src/app/eventos/eventos.component.ts
+++ Front/Agenda-app/src/app/eventos/eventos.component.ts
@@ -7,19 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './eventos.component.scss'
 })
 export class EventosComponent implements OnInit {
-public eventos: any ;
+public eventos: any = [];
+public eventosFiltrados: any = [];
+public exibirImg: boolean = true;
+private _filtroLista: string = '';
+
+public get filtroLista(): string{
+  return this._filtroLista
+}
+
+public set filtroLista(value: string){
+  this._filtroLista = value;
+  this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+}
+
+filtrarEventos(filtrarPor: string): any{
+   filtrarPor = filtrarPor.toLocaleLowerCase();
+  return this.eventos.filter(
+    (x: any) => x.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+    x.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+  )
+}
 
 constructor( private http: HttpClient){}
-ngOnInit(): void {
-  this.ObterEventos();
+  ngOnInit(): void {
+    this.obterEventos();
 
-}
+  }
 
-ObterEventos() {
-  this.http.get('http://localhost:5000/api/eventos').subscribe((result) => {
-    console.log(result);
-    this.eventos = result;
-  })
+  obterEventos() {
+    this.http.get('http://localhost:5000/api/eventos').subscribe(
+      (result) => {
+        this.eventos = result;
+        this.eventosFiltrados = result;
+      }, (error) => {
+        console.log('Erro ao obter eventos:', error)
+      }
+    );
+  }
 
-}
+  mostrarImg() {
+    this.exibirImg = !this.exibirImg
+  }
 }
